45976fbd00988180c68422dd0ee7bf71e6953338
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-05-10 19:16:40 +0200

    Removed engine time scaling on player death

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,11 +3,8 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(_body):
-	print("Player fall of the map!")
-	Engine.time_scale = 0.5
 	timer.start()
 
 func _on_timer_timeout():
 	# Reset game
-	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
